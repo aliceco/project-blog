f7628296e48b4bfc52558307cb229699da64d866
aliceco <aliceco@kth.se>
2026-06-02 10:05:44 +0200

    Fixed footer

---
+++ db-credentials.php
@@ -0,0 +1,11 @@
+<?php
+const DB_SERVER = "localhost";
+const DB_USER   = "alicoh5";
+const DB_PASS   = "kzs5vgvfifpcUzxi";
+const DB_NAME   = "alicoh5"; 
+
+
+// const DB_SERVER = "localhost";
+// const DB_USER   = "root";
+// const DB_PASS   = "";
+// const DB_NAME   = "blog";
